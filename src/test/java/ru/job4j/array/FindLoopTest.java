1e23bc4e90399b1c59e6b899e7423fc072bbc11f
Mingrow <nikitosvozh@gmail.com>
2023-02-20 21:19:40 +0300

    6.5. Поиск индекса в диапазоне. [#156318]

--- src/test/java/ru/job4j/array/FindLoopTest.java
+++ src/test/java/ru/job4j/array/FindLoopTest.java
@@ -31,4 +31,58 @@ class FindLoopTest {
         assertThat(result).isEqualTo(expected);
     }
 
+    @Test
+    public void whenDiapasonHas8ThenResultEqualFinish() {
+        int[] data = new int[] {5, 2, 10, 2, 4, 8, 14, 3, 21, 16};
+        int el = 8;
+        int start = 2;
+        int finish = 5;
+        int result = FindLoop.indexInRange(data, el, start, finish);
+        int expected = 5;
+        assertThat(result).isEqualTo(expected);
+    }
+
+    @Test
+    public void whenDiapasonHasSomeEqualElementThen3() {
+        int[] data = new int[] {5, 10, 2, 4, 8, 4, 14, 4, 3, 21, 16};
+        int el = 4;
+        int start = 1;
+        int finish = 8;
+        int result = FindLoop.indexInRange(data, el, start, finish);
+        int expected = 3;
+        assertThat(result).isEqualTo(expected);
+    }
+
+    @Test
+    public void whenDiapasonHas2Then3() {
+        int[] data = new int[] {5, 2, 10, 2, 4};
+        int el = 2;
+        int start = 2;
+        int finish = 4;
+        int result = FindLoop.indexInRange(data, el, start, finish);
+        int expected = 3;
+        assertThat(result).isEqualTo(expected);
+    }
+
+    @Test
+    public void whenDiapasonHasNot8ThenMinus1() {
+        int[] data = new int[] {1, 4, 11, 25};
+        int el = 8;
+        int start = 1;
+        int finish = 3;
+        int result = FindLoop.indexInRange(data, el, start, finish);
+        int expect = -1;
+        assertThat(result).isEqualTo(expect);
+    }
+
+    @Test
+    public void whenDiapasonHas10Then5() {
+        int[] data = new int[] {1, 4, 11, 25, 100, 10, 23, 14};
+        int el = 10;
+        int start = 3;
+        int finish = 6;
+        int result = FindLoop.indexInRange(data, el, start, finish);
+        int expect = 5;
+        assertThat(result).isEqualTo(expect);
+    }
 }
