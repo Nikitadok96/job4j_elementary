70b27d698a9631aaf01e63625145cc0b691a6b4c
Mingrow <nikitosvozh@gmail.com>
2023-02-26 15:30:13 +0300

    4. Расстояние между точками в трехмерном пространстве. [#122643]

--- src/main/java/ru/job4j/condition/Point.java
+++ src/main/java/ru/job4j/condition/Point.java
@@ -3,10 +3,21 @@ package ru.job4j.condition;
 public class Point {
     private int x;
     private int y;
+    private int z;
 
     public Point(int first, int second) {
         this.x = first;
         this.y = second;
+    }
+
+    public Point(int first, int second, int third) {
+        this.x = first;
+        this.y = second;
+        this.z = third;
+    }
+
+    public double distance3d(Point that) {
+        return Math.sqrt(Math.pow(that.x - this.x, 2) + Math.pow(that.y - this.y, 2) + Math.pow(that.z - this.z, 2));
     }
 
     public double distance(Point that) {
--- src/test/java/ru/job4j/condition/PointTest.java
+++ src/test/java/ru/job4j/condition/PointTest.java
@@ -2,6 +2,8 @@ package ru.job4j.condition;
 
 import org.junit.Assert;
 import org.junit.jupiter.api.Test;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.offset;
 
 class PointTest {
     @Test
@@ -29,5 +31,23 @@ class PointTest {
         double expected = 0;
         double rsl = a.distance(b);
         Assert.assertEquals(expected, rsl, 0.01);
+    }
+
+    @Test
+    public void when000To003Then3() {
+        Point a = new Point(0, 0, 0);
+        Point b = new Point(0, 0, 3);
+        double expected = 3;
+        double rsl = a.distance3d(b);
+        assertThat(rsl).isCloseTo(expected, offset(0.00001));
+    }
+
+    @Test
+    public void when010To110Then1() {
+        Point a = new Point(0, 1, 0);
+        Point b = new Point(1, 1, 0);
+        double expected = 1;
+        double rsl = a.distance3d(b);
+        assertThat(rsl).isCloseTo(expected, offset(0.00001));
     }
 }
